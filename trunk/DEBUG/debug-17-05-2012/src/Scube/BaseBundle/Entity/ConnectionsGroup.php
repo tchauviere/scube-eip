71c8a858966ae452ef97e91e588b3bab2a657850
beauferey.kevin <beauferey.kevin@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-06-15 22:07:38 +0000

    Added forgotten class

---
+++ trunk/DEBUG/debug-17-05-2012/src/Scube/BaseBundle/Entity/ConnectionsGroup.php
@@ -0,0 +1,86 @@
+<?php
+// src/Scube/BaseBundle/Entity/ConnectionsGroup.php
+namespace Scube\BaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="connections_group")
+ */
+class ConnectionsGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+	
+	/**
+     * @ORM\ManyToMany(targetEntity="Scube\BaseBundle\Entity\User")
+	 * @ORM\JoinTable(name="connections_group_users")
+     */
+    protected $users;
+
+	public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add users
+     *
+     * @param Scube\BaseBundle\Entity\User $users
+     */
+    public function addUser(\Scube\BaseBundle\Entity\User $users)
+    {
+        $this->users[] = $users;
+    }
+
+    /**
+     * Get users
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+}
