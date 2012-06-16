0f403f10f7e02e3877b4a96725816c0f7d141212
cedric.laverdure@gmail.com <cedric.laverdure@gmail.com@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-06-16 07:04:45 +0000

    Update ManageFileBundle

---
+++ DEBUG/debug-17-05-2012/src/Scube/ManageFileBundle/Entity/Picture.php
@@ -0,0 +1,17 @@
+<?php
+namespace Scube\ManageFileBundle\Entity;
+
+class Picture
+{
+	protected $file;
+
+	public function getFile()
+	{
+		return $this->file;
+	}
+	
+	public function setFile($file)
+	{
+		$this->file = $file;
+	}
+}
