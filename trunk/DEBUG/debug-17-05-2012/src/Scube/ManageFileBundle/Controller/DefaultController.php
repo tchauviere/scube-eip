e5a7507c3d9a094143bbbab6db6223a7d8f6bed9
cedric.laverdure@gmail.com <cedric.laverdure@gmail.com@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-06-16 07:04:45 +0000

    Update ManageFileBundle

--- trunk/DEBUG/debug-17-05-2012/src/Scube/ManageFileBundle/Controller/DefaultController.php
+++ trunk/DEBUG/debug-17-05-2012/src/Scube/ManageFileBundle/Controller/DefaultController.php
@@ -8,38 +8,47 @@ use Scube\ManageFileBundle\Entity\File;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name = "toto")
-    {
-        return $this->redirect($this->generateUrl('ScubeManageFileBundle_pictures_show'));
-    }
-    
+
+	public function indexAction($name = "toto")
+	{
+		return $this->redirect($this->generateUrl('ScubeManageFileBundle_upload'));
+	}
+
 	public function uploadAction(Request $request)
-	{		
+	{
 		$userID = "1234";
 		$picturesPath = "Medias/Pictures/$userID/";
 		$pdfPath = "Medias/PDF/$userID/";
+		$videosPath = "Medias/Videos/$userID/";
 		$file = new File();
 
 		$form = $this->createFormBuilder($file)
 		->add('file', 'file')
 		->getForm();
-		
+
 		if ($request->getMethod() == 'POST') {
 			$form->bindRequest($request);
 			if ($form->isValid()) {
+				$mimeType = $form['file']->getData()->getMimeType();
 				$extension = $form['file']->getData()->guessExtension();
-				if ($extension == 'pdf')
+				if ($mimeType == 'application/pdf' || $mimeType == 'application/x-pdf') {
 					$form['file']->getData()->move($pdfPath, rand().'.'.$extension);
-				else
- 					$form['file']->getData()->move($picturesPath, rand().'.'.$extension);
-				return $this->redirect($this->generateUrl('ScubeManageFileBundle_pictures_show'));
+					return $this->redirect($this->generateUrl('ScubeManageFileBundle_show_pdf'));
+				}
+				else if ($mimeType == 'image/png' || $mimeType == 'image/jpg' || $mimeType == 'image/jpeg') {
+					$form['file']->getData()->move($picturesPath, rand().'.'.$extension);
+					return $this->redirect($this->generateUrl('ScubeManageFileBundle_show_pictures'));
+				}
+				else {
+					$form['file']->getData()->move($videosPath, rand().'.'.$extension);
+					return $this->redirect($this->generateUrl('ScubeManageFileBundle_show_videos'));
+				}
 			}
 		}
-		
+
 		return $this->render('ScubeManageFileBundle:Default:upload.html.twig', array('form' => $form->createView()));
 	}
-	
+
 	public function deleteAction(Request $request)
 	{
 		if ($request->getMethod() == 'POST') {
@@ -50,13 +59,13 @@ class DefaultController extends Controller
 					$filename = $filename[count($filename) - 2].'/'.$filename[count($filename) - 1];
 					unlink('Medias/Pictures/'.$filename);
 				}
-				return $this->redirect($this->generateUrl('ScubeManageFileBundle_pictures_show'));
+				return $this->redirect($this->generateUrl('ScubeManageFileBundle_show_pictures'));
 			}
 		}
-		
+
 		$userID = "1234";
 		$filePath = "Medias/Pictures/$userID";
-		
+
 		$list = array();
 		$handle = @dir($filePath);
 
@@ -68,31 +77,14 @@ class DefaultController extends Controller
 
 		return $this->render('ScubeManageFileBundle:Default:delete.html.twig', array('list' => $list));
 	}
-	
-	public function showAction(Request $request)
+
+	public function showPdfAction(Request $request)
 	{
-		// PICTURES
-		
-		$userID = "1234";
-		$filePath = "Medias/Pictures/$userID";
-		
-		$list = array();
-		$handle = @dir($filePath);
-
-		$i = 0;
-		while ($entry = $handle->read()) {
-			if (!@is_dir($entry))
-				$list[$i++] = $this->get('request')->getBasePath()."/$filePath/$entry";
-
-		}
-		return $this->render('ScubeManageFileBundle:Default:show.html.twig', array('list' => $list, 'server_addr' => $_SERVER['HTTP_HOST']));
-		
-		// PDF
-		/*
 		$userID = "1234";
 		$filePath = "Medias/PDF/$userID";
-		
+
 		$list = array();
+		$list[0] = "";
 		$handle = @dir($filePath);
 
 		$i = 0;
@@ -101,7 +93,39 @@ class DefaultController extends Controller
 				$list[$i++] = $this->get('request')->getBasePath()."/$filePath/$entry";
 		}
 		return $this->render('ScubeManageFileBundle:Default:pdf.html.twig', array('pdf_path' => $list[0]));
-		*/
+		
+	}
+
+	public function showPicturesAction(Request $request)
+	{
+		$userID = "1234";
+		$filePath = "Medias/Pictures/$userID";
+
+		$list = array();
+		$handle = @dir($filePath);
+
+		$i = 0;
+		while ($entry = $handle->read()) {
+		if (!@is_dir($entry))
+			$list[$i++] = $this->get('request')->getBasePath()."/$filePath/$entry";
+		}
+		return $this->render('ScubeManageFileBundle:Default:pictures.html.twig', array('list' => $list, 'server_addr' => $_SERVER['HTTP_HOST']));
+	}
+
+	public function showVideosAction(Request $request)
+	{
+		$userID = "1234";
+		$filePath = "Medias/Videos/$userID";
+
+		$list = array();
+		$handle = @dir($filePath);
+
+		$i = 0;
+		while ($entry = $handle->read()) {
+		if (!@is_dir($entry))
+			$list[$i++] = $this->get('request')->getBasePath()."/$filePath/$entry";
+		}
+		return $this->render('ScubeManageFileBundle:Default:videos.html.twig', array('list' => $list));
 	}
 	
 }
