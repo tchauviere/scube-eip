ebd0b6595bc52f9b990806e28878917f5d8479d1
beauferey.kevin <beauferey.kevin@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-07-17 18:37:24 +0000

    Deleted SimpleClock, Added Clock

--- trunk/DEBUG/debug-17-05-2012/WIDGETS/src/ClockBundle/DependencyInjection/Configuration.php
+++ trunk/DEBUG/debug-17-05-2012/WIDGETS/src/ClockBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Widgets\SimpleClockBundle\DependencyInjection;
+namespace Widgets\ClockBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('widgets_simple_clock');
+        $rootNode = $treeBuilder->root('widgets_clock');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- trunk/DEBUG/debug-17-05-2012/WIDGETS/src/ClockBundle/Tests/Controller/DefaultControllerTest.php
+++ trunk/DEBUG/debug-17-05-2012/WIDGETS/src/ClockBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Widgets\SimpleClockBundle\Tests\Controller;
+namespace Widgets\ClockBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
