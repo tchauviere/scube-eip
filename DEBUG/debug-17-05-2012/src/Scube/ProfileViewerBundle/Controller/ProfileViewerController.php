62746fbf6e8d053607af404b0e19c70686694496
beauferey.kevin <beauferey.kevin@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-07-18 09:32:57 +0000

    Check permissions in profileviewer (news, infos, pics)

--- DEBUG/debug-17-05-2012/src/Scube/ProfileViewerBundle/Controller/ProfileViewerController.php
+++ DEBUG/debug-17-05-2012/src/Scube/ProfileViewerBundle/Controller/ProfileViewerController.php
@@ -26,7 +26,30 @@ class ProfileViewerController extends Controller
 		$user_connected = $repository->findOneBy(array('email' => $session->get('user')->getEmail(), 'password' => $session->get('user')->getPassword()));
 		$user_to_display = $repository->find($id_user);
 		
-        return $this->render('ScubeProfileViewerBundle:ProfileViewer:newsfeed.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display));
+		if ($user_connected == $user_to_display)
+			$auth = true;
+		else
+		{
+			$groups = $user_to_display->getConnectionsGroups();
+			$auth = false;
+			foreach ($groups as $grp)
+			{
+				if ( ! $grp->getAuthProfileNews())
+					continue;
+				
+				foreach ($grp->getUsers() as $usr)
+				{
+					if ($user_connected == $usr)
+					{
+						$auth = true;
+						break ;
+					}
+				}
+				if ($auth)
+					break ;
+			}
+		}
+        return $this->render('ScubeProfileViewerBundle:ProfileViewer:newsfeed.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display, "auth"=>$auth));
     }
 	public function infosAction($id_user)
     {
@@ -35,7 +58,31 @@ class ProfileViewerController extends Controller
 		$user_connected = $repository->findOneBy(array('email' => $session->get('user')->getEmail(), 'password' => $session->get('user')->getPassword()));
 		$user_to_display = $repository->find($id_user);
 		
-        return $this->render('ScubeProfileViewerBundle:ProfileViewer:infos.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display));
+		if ($user_connected == $user_to_display)
+			$auth = true;
+		else
+		{
+			$groups = $user_to_display->getConnectionsGroups();
+			$auth = false;
+			foreach ($groups as $grp)
+			{
+				if ( ! $grp->getAuthProfileInfos())
+					continue;
+				
+				foreach ($grp->getUsers() as $usr)
+				{
+					if ($user_connected == $usr)
+					{
+						$auth = true;
+						break ;
+					}
+				}
+				if ($auth)
+					break ;
+			}
+		}
+		
+        return $this->render('ScubeProfileViewerBundle:ProfileViewer:infos.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display, "auth"=>$auth));
     }
 	public function picsAction($id_user)
     {
@@ -44,6 +91,30 @@ class ProfileViewerController extends Controller
 		$user_connected = $repository->findOneBy(array('email' => $session->get('user')->getEmail(), 'password' => $session->get('user')->getPassword()));
 		$user_to_display = $repository->find($id_user);
 		
-        return $this->render('ScubeProfileViewerBundle:ProfileViewer:pics.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display));
+		if ($user_connected == $user_to_display)
+			$auth = true;
+		else
+		{
+			$groups = $user_to_display->getConnectionsGroups();
+			$auth = false;
+			foreach ($groups as $grp)
+			{
+				if ( ! $grp->getAuthProfilePics())
+					continue;
+				
+				foreach ($grp->getUsers() as $usr)
+				{
+					if ($user_connected == $usr)
+					{
+						$auth = true;
+						break ;
+					}
+				}
+				if ($auth)
+					break ;
+			}
+		}
+		
+        return $this->render('ScubeProfileViewerBundle:ProfileViewer:pics.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display, "auth"=>$auth));
     }
 }
