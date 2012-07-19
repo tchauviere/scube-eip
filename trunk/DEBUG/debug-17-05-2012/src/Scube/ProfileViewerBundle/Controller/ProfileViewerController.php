67f1f1aa4fb1e9f30007a04250269dcac5b94fc7
thibaud.chauviere <thibaud.chauviere@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-07-19 14:40:22 +0000

    Update Version Mobile avec base flux

--- trunk/DEBUG/debug-17-05-2012/src/Scube/ProfileViewerBundle/Controller/ProfileViewerController.php
+++ trunk/DEBUG/debug-17-05-2012/src/Scube/ProfileViewerBundle/Controller/ProfileViewerController.php
@@ -17,7 +17,8 @@ class ProfileViewerController extends Controller
 		$repository = $this->getDoctrine()->getRepository('ScubeBaseBundle:User');
 		$user_connected = $repository->findOneBy(array('email' => $session->get('user')->getEmail(), 'password' => $session->get('user')->getPassword()));
 		$user_to_display = $repository->find($id_user);
-		
+		if (\Scube\BaseBundle\Controller\BaseController::isMobile())
+			return $this->render('ScubeBaseBundle:Base_Mobile:view_profile.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display));
         return $this->render('ScubeProfileViewerBundle:ProfileViewer:index.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display));
     }
 	public function newsfeedAction(Request $request, $id_user)
@@ -72,7 +73,8 @@ class ProfileViewerController extends Controller
 							   return $this->redirect($this->generateUrl('ScubeProfileViewerBundle_homepage', array("id_user"=>$user_to_display->getId())));
                        }
                }
-		
+		if (\Scube\BaseBundle\Controller\BaseController::isMobile())
+			return $this->render('ScubeBaseBundle:Base_Mobile:news_feed.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display, "auth"=>$auth, 'form' => $form->createView(), "success"=>false));
         return $this->render('ScubeProfileViewerBundle:ProfileViewer:newsfeed.html.twig', array("user_connected"=>$user_connected, "user_to_display"=>$user_to_display, "auth"=>$auth, 'form' => $form->createView(), "success"=>false));
     }
 	public function newsfeedRemoveAction(Request $request, $id_user, $id_news)
