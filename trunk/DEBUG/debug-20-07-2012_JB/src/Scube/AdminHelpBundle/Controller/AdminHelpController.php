9e75c2234ae2172e6bd7fd0c05234ae49e13370d
valfar777@gmail.com <valfar777@gmail.com@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-10-08 18:51:37 +0000

    working version of AdminHelpBundle
    need .html.twig files & translations

--- trunk/DEBUG/debug-20-07-2012_JB/src/Scube/AdminHelpBundle/Controller/AdminHelpController.php
+++ trunk/DEBUG/debug-20-07-2012_JB/src/Scube/AdminHelpBundle/Controller/AdminHelpController.php
@@ -3,13 +3,42 @@
 namespace Scube\AdminHelpBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Scube\BaseBundle\Entity\Application;
+use Scube\CoreBundle\Controller\CoreController;
 
 
-class AdminHelpController extends Controller
+class AdminHelpController extends CoreController
 {
-    
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('ScubeAdminHelpBundle:AdminHelp:index.html.twig', array());
+		$this->preprocessApplication();
+		$em = $this->getDoctrine()->getEntityManager();
+		$app_list = $this->getMenu();
+        return $this->render('ScubeAdminHelpBundle:AdminHelp:index.html.twig', array('app_list'=>$app_list));
     }
+	
+	public function getHelpAction(Request $request, $id)
+	{
+		$this->preprocessApplication();
+		$em = $this->getDoctrine()->getEntityManager();
+		$app_list = $this->getMenu();
+
+		if (file_exists($this->get('kernel')->getRootDir()."/../src/Scube/" . $id ."/Resources/views/". substr($id, 0, strlen($id) - strlen("Bundle")) . "/help.html.twig"))
+		{
+				$helpPath = "Scube" . $id . ":" . substr($id, 0, strlen($id) - strlen("Bundle")) . ":help.html.twig";
+		}
+		else
+			$helpPath = "ScubeAdminHelpBundle:AdminHelp:noHelp.html.twig";
+        return $this->render('ScubeAdminHelpBundle:AdminHelp:displayHelp.html.twig', array('helpFile'=>$helpPath, 'app_list'=>$app_list));
+	}
+	
+	private function getMenu()
+	{
+		$em = $this->getDoctrine()->getEntityManager();
+		$query = $em->createQuery("SELECT a FROM ScubeBaseBundle:Application a ORDER BY a.type, a.name ASC");
+		$app_list = $query->getResult();
+		return 	$app_list;
+	}
 }
