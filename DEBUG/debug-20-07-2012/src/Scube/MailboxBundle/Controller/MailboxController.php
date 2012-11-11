8bf872f95934200433933707baa30925466caf1c
beauferey.kevin <beauferey.kevin@fc5c6361-b3a0-e799-2a36-3915703afc89>
2012-11-11 15:58:42 +0000

    Updated bug on empty conversation list for mailbox

--- DEBUG/debug-20-07-2012/src/Scube/MailboxBundle/Controller/MailboxController.php
+++ DEBUG/debug-20-07-2012/src/Scube/MailboxBundle/Controller/MailboxController.php
@@ -99,7 +99,10 @@ class MailboxController extends CoreController
 		{
 			$em = $this->getDoctrine()->getEntityManager();
 			$query = $em->createQuery("SELECT u, m, c FROM ScubeBaseBundle:User u JOIN u.mailbox m JOIN m.conversations c WHERE u.id=:id_user ORDER BY c.date_last_mail DESC")->setParameters(array('id_user' => $user->getId()));
-			$conversations = $query->getResult()[0]->getMailbox()->getConversations();
+			$conv_result = $query->getResult();
+			$conversations = array();
+			if ($conv_result)
+				$conversations = $query->getResult()[0]->getMailbox()->getConversations();
 		}
 
         return $this->render('ScubeMailboxBundle:Mailbox:index.html.twig', array('user' => $user, 'conversation' => false, 'all_users' => $all_users, 'conversations_list' => $conversations));
